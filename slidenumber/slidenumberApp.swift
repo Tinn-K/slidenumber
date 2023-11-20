974c3e8d1f49b95ea4cc99a4b135feb0266f3910
Tinn-K <tinn.kan@dome.tu.ac.th>
2023-11-20 16:57:06 +0700

    Initial Commit

---
+++ slidenumber/slidenumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  slidenumberApp.swift
+//  slidenumber
+//
+//  Created by KnoWhere on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct slidenumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
